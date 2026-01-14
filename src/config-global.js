3f82d2a2bfdd3b66f234563105d84f210b572cc4
developer11technolitics <dev11.technolitics@gmail.com>
2026-01-14 09:22:47 +0530

    pushed

--- src/config-global.js
+++ src/config-global.js
@@ -4,8 +4,8 @@ import { PATH_DASHBOARD } from './routes/paths';
 // API
 // ----------------------------------------------------------------------
 
-// export const HOST_API_KEY = 'https://rupaapi.aishkhan.in';
-export const HOST_API_KEY = 'http://localhost:3000';
+export const HOST_API_KEY = 'https://rupaapi.aishkhan.in';
+// export const HOST_API_KEY = 'http://localhost:3000';
 
 export const FIREBASE_API = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
